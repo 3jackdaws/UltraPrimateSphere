e5cc96c330eb905dfbdb78ce0e8d8c96577e0c01
Ian Murphy <3jackdaws@gmail.com>
2016-04-05 20:37:49 -0700

    Main menu is in pretty good shape

---
+++ Assets/Scripts/userInterface/TitleBall.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleBall : MonoBehaviour
+{
+    private GameObject _ball;
+
+    void Start ()
+    {
+        _ball = gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.transform.rotation = Quaternion.AngleAxis(0.5f, Vector3.left) * transform.rotation;
+    }
+
+}
